abc898777d60e92ddc8daed8e1625f9de5e1c784
sanskar-agrawal1903 <59992723+sanskar-agrawal1903@users.noreply.github.com>
2020-10-19 11:08:54 +0530

    Update code.go

--- code.go
+++ code.go
@@ -77,31 +77,12 @@ func logoutHandler(response http.ResponseWriter, request *http.Request) {
 }
 
 
-const indexPage = `
-<h1>Login</h1>
-<form method="post" action="/login">
-    <label for="name">User name</label>
-    <input type="text" id="name" name="name">
-    <label for="password">Password</label>
-    <input type="password" id="password" name="password">
-    <button type="submit">Login</button>
-</form>
-`
 
 func indexPageHandler(response http.ResponseWriter, request *http.Request) {
 	fmt.Fprintf(response, indexPage)
 }
 
-// internal page
 
-const internalPage = `
-<h1>Internal</h1>
-<hr>
-<small>User: %s</small>
-<form method="post" action="/logout">
-    <button type="submit">Logout</button>
-</form>
-`
 
 func internalPageHandler(response http.ResponseWriter, request *http.Request) {
 	userName := getUserName(request)
